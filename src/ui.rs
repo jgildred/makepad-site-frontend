10983519645c931bb023004f630bfaf01df17595
John Gildred <274078+jgildred@users.noreply.github.com>
2024-07-28 21:16:05 -0700

    noop

--- src/ui.rs
+++ src/ui.rs
@@ -18,7 +18,7 @@ live_design!(
     ICO_LIKES = dep("crate://self/resources/icon_likes.svg")
     ICO_USER = dep("crate://self/resources/icon_user.svg")
     ICO_ADD = dep("crate://self/resources/icon_add.svg")
-        
+    
     FONT_SIZE_SUB = 9.5
     FONT_SIZE_P = 12.5
         
